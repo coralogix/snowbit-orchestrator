8c6c4d30668447bd23e22495dd1f5b2f6bc96fae
Mahesh M <mahesh.m@antstack.io>
2022-07-04 23:30:21 +0530

    added exception handlers

--- orchestrator_codebase/validate.py
+++ orchestrator_codebase/validate.py
@@ -5,6 +5,7 @@ class Severity(enum.Enum):
     LOW = 0
     MID = 1
     HIGH = 2
+    CRITICAL = 3
 
 
 class Validate:
@@ -13,11 +14,15 @@ class Validate:
 
     def localize(self):
         flag = 0
-        if isinstance(self.p_data['accountId'], int) and len(self.p_data['accountId']) == 12:
-            if isinstance(self.p_data['alertDescription'], str) and len(self.p_data['alertDescription']) == 31:
-                if isinstance(self.p_data['alertName'], str) and len(self.p_data['alertName']) == 120:
-                    if isinstance(self.p_data['alertSeverity'], enum.EnumMeta) and (self.p_data['alertSeverity']) in type(self.p_data['alertSeverity'].__name__):
-                        flag = 1
+        try:
+            if isinstance(self.p_data['accountId'], int) and len(self.p_data['accountId']) == 12:
+                if isinstance(self.p_data['alertDescription'], str) and len(self.p_data['alertDescription']) >= 31:
+                    if isinstance(self.p_data['alertName'], str) and len(self.p_data['alertName']) == 120:
+                        if isinstance(self.p_data['alertSeverity'], enum.EnumMeta) and (self.p_data['alertSeverity']) in type(self.p_data['alertSeverity'].__name__):
+                            flag = 1
+        except KeyError:
+            # should this error be logged using boto3 to cloudwatch ?
+            pass
 
         if flag == 1:
             return "VALID"
